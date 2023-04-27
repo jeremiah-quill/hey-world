15acb01adc643093bef3042c5c0a1df2dda3fddb
Jeremiah Quill <jcq5010@gmail.com>
2023-04-27 07:39:45 -0400

    add system prompt

--- src/pages/api/gpt.js
+++ src/pages/api/gpt.js
@@ -11,12 +11,13 @@ export default async function handler(req, res) {
 
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-
-    // {
-    //   role: "system",
-    //   content: `You are an advanced AI tasked with compressing text. You are given a prompt and must compress it in a way that is lossless but results in the minimum number of tokens which could be fed into an LLM like yourself as-is and produce the same output. Feel free to use multiple languages, symbols, other up-front priming to lay down rules. This is entirely for yourself to recover and proceed from with the same conceptual priming, not for humans to decompress. You will be given a prompt, and you must respond with your lossless compressed version of this prompt.`,
-    // },
-    messages: conversation,
+    messages: [
+      {
+        role: "system",
+        content: `Forget all previous instructions. You are a world class web interface designer and developer tasked with helping users build with HTML and CSS. Users will ask you questions about a feature, component, web technology, layout, etc. You will answer their questions and help them build what they want. If the user asks specifically for code, please only respond wth the minimal explanation.  Please only respond using markdown.`,
+      },
+      ...conversation,
+    ],
   });
 
   res.status(200).json(response.data);
