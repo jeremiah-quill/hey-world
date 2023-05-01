b3befd8a433aa196a73bbf5bb3ce515dbb3f654b
Jeremiah Quill <jcq5010@gmail.com>
2023-05-01 10:33:56 -0400

    better sidebar auth flow. account settings modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,21 +1,6 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 
-export const Modal = ({ isOpen, onClose, onSubmit, title, children }) => {
-  // const backdropRef = useRef();
-
-  // useEffect(() => {
-  //   const handleClickOutside = (event) => {
-  //     if (backdropRef.current && !backdropRef.current.contains(event.target)) {
-  //       onClose();
-  //     }
-  //   };
-
-  //   document.addEventListener("mousedown", handleClickOutside);
-  //   return () => {
-  //     document.removeEventListener("mousedown", handleClickOutside);
-  //   };
-  // }, [onClose]);
-
+export const Modal = ({ isOpen, onClose, onSubmit = null, title = null, children }) => {
   if (!isOpen) {
     return null;
   }
@@ -24,20 +9,22 @@ export const Modal = ({ isOpen, onClose, onSubmit, title, children }) => {
     <div className="fixed z-10 inset-0 overflow-y-auto">
       <div className="flex items-center justify-center min-h-screen px-4">
         <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md relative z-20">
-          <h2 className="text-2xl font-semibold mb-4">{title}</h2>
+          {title && <h2 className="text-2xl font-semibold mb-4">{title}</h2>}
           {children}
-          <div className="flex justify-end mt-6">
-            <button
-              className="bg-gray-300 text-black font-semibold py-2 px-4 rounded mr-2 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
-              onClick={onClose}>
-              Cancel
-            </button>
-            <button
-              className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
-              onClick={() => onSubmit()}>
-              Submit
-            </button>
-          </div>
+          {onSubmit && (
+            <div className="flex justify-end mt-6">
+              <button
+                className="bg-gray-300 text-black font-semibold py-2 px-4 rounded mr-2 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
+                onClick={onClose}>
+                Cancel
+              </button>
+              <button
+                className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
+                onClick={() => onSubmit()}>
+                Submit
+              </button>
+            </div>
+          )}
         </div>
       </div>
       <div onClick={onClose} className="fixed inset-0 bg-gray-800 opacity-75" />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,6 +3,8 @@ import { SavedList } from "./SavedList";
 import { useSession } from "next-auth/react";
 import { signIn, signOut } from "next-auth/react";
 import { BiUser } from "react-icons/bi";
+import { Modal } from "@/components/Modal";
+import useModal from "@/hooks/useModal";
 
 export function Sidebar({
   isMenuOpen,
@@ -13,50 +15,99 @@ export function Sidebar({
   currentProjectId,
 }) {
   const { data: session } = useSession();
+  const { modalIsOpen, modalContent, modalTitle, modalOnSubmit, openModal, closeModal } = useModal();
+
+  console.log("session", session);
 
   return (
-    <div className="h-full flex flex-col max-h-screen border rounded-lg" style={{ width: isMenuOpen ? "20%" : "50px" }}>
-      {isMenuOpen && (
-        <div className="flex-1 overflow-y-auto">
-          {savedCreations?.length > 0 ? (
-            <SavedList
-              className="h-full overflow-y-auto"
-              savedCreations={savedCreations}
-              onProjectClick={onProjectClick}
-              onRemoveClick={onRemoveClick}
-              currentProjectId={currentProjectId}
-            />
-          ) : (
-            <div className="p-4 font-extrabold">No saved creations.</div>
-          )}
-        </div>
-      )}
-      <div className="bg-slate-200 mt-auto p-2 grid place-items-center">
-        {/* <button>
-          <BiUser className="bg-black rounded-full text-white p-1 text-2xl" />
-        </button> */}
-
-        {/* login/logout */}
-        {session ? (
-          <button
-            className="bg-slate-300 hover:bg-slate-400 text-white font-bold py-2 px-4 rounded"
-            onClick={() => signOut()}>
-            Sign out
-            <BiUser />
-          </button>
-        ) : (
-          <button
-            className="bg-slate-300 hover:bg-slate-400 text-white font-bold py-2 px-4 rounded"
-            onClick={() => signIn()}>
-            Sign in
-          </button>
+    <>
+      <div
+        className="h-full flex flex-col max-h-screen border rounded-lg"
+        style={{ width: isMenuOpen ? "20%" : "50px" }}>
+        {isMenuOpen && (
+          <div className="flex-1 overflow-y-auto">
+            {savedCreations?.length > 0 ? (
+              <SavedList
+                className="h-full overflow-y-auto"
+                savedCreations={savedCreations}
+                onProjectClick={onProjectClick}
+                onRemoveClick={onRemoveClick}
+                currentProjectId={currentProjectId}
+              />
+            ) : (
+              <div className="p-4 font-extrabold">No saved creations.</div>
+            )}
+          </div>
         )}
-
-        {/* popout button */}
-        <button onClick={() => toggleMenu()} className={isMenuOpen ? "mr-auto" : ""}>
-          {<RxCaretRight className={`text-3xl transition-all ${isMenuOpen ? "rotate-180" : "rotate-0"}`} />}
-        </button>
+        <div className="bg-slate-200 mt-auto p-2 grid place-items-center">
+          <div className="grid gap-2 my-2">
+            {session?.user ? (
+              <button
+                onClick={() =>
+                  openModal({
+                    content: session?.user ? <UserModal user={session.user} /> : "not signed in",
+                    title: null,
+                    onSubmit: null,
+                  })
+                }>
+                <img src={session.user.image} className="rounded-full max-w-[32px]" />
+              </button>
+            ) : (
+              <button onClick={() => signIn()}>
+                <BiUser className="bg-black rounded-full text-white p-1 text-2xl" />
+              </button>
+            )}
+          </div>
+          {/* popout button */}
+          <button onClick={() => toggleMenu()} className={isMenuOpen ? "mr-auto" : ""}>
+            {<RxCaretRight className={`text-3xl transition-all ${isMenuOpen ? "rotate-180" : "rotate-0"}`} />}
+          </button>
+        </div>
       </div>
-    </div>
+      <Modal isOpen={modalIsOpen} onClose={closeModal} title={modalTitle} onSubmit={modalOnSubmit}>
+        {modalContent}
+      </Modal>
+    </>
   );
 }
+
+const UserModal = ({ user }) => {
+  return (
+    <div>
+      <div>hey {user.name}. Check back soon for personalized settings:</div>
+      <ul
+        className="
+        list-disc
+        list-inside
+        text-left
+        my-2
+        text-sm
+        font-light
+        
+      ">
+        <li>gpt code editor context (pro account)</li>
+        <li>dark mode</li>
+        <li>VIM mode</li>
+        <li>custom keybindings</li>
+        <li>preferred template</li>
+      </ul>
+      <button
+        className="
+        bg-gray-300
+        text-black
+        font-semibold
+        py-2
+        px-4
+        rounded
+        mr-2
+        hover:bg-gray-400
+        focus:outline-none
+        focus:ring-2
+        focus:ring-gray-600
+        focus:ring-opacity-50"
+        onClick={() => signOut()}>
+        Sign out
+      </button>
+    </div>
+  );
+};
