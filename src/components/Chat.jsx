f36fd1f8d4400c032792239bc73fcf31efb2a475
Jeremiah Quill <jcq5010@gmail.com>
2023-05-03 15:02:55 -0400

    extract chat settings logic to hook

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import { FaKey } from "react-icons/fa";
 import { signIn, useSession } from "next-auth/react";
@@ -6,8 +6,8 @@ import { useUserSettings } from "@/context/userSettingsContext";
 import { ChatError } from "@/components/ChatError";
 import useModal from "@/hooks/useModal";
 import { Modal } from "@/components/Modal";
-import { useToast } from "@/context/toastContext";
 import { useChat } from "@/hooks/useChat";
+import { useChatSettings } from "@/hooks/useChatSettings";
 
 export function Chat({ isChatOpen, messages, setMessages }) {
   //  modal state, handlers, and effects
@@ -124,76 +124,20 @@ export function Chat({ isChatOpen, messages, setMessages }) {
   );
 }
 
-const ChatSettingsModal = ({ onClose, messages, setMessages }) => {
+const ChatSettingsModal = ({ onClose }) => {
   // state
   const { data: session } = useSession();
-  const [loading, setLoading] = useState(false);
-  const { userSettings, toggleSetting, key, syncKey } = useUserSettings();
-  const [inputValue, setInputValue] = useState(key || "");
-  const [selectedOption, setSelectedOption] = useState(
-    !session ? "user" : userSettings.isUseUserKeyEnabled ? "user" : "free"
-  );
+  const { userSettings } = useUserSettings();
 
-  const showToast = useToast();
-
-  // handlers
-  function handleChange(e) {
-    setInputValue(e.target.value);
-  }
-  const handleClick = (option) => {
-    setSelectedOption(option);
-  };
-  async function handleSubmit(e) {
-    setLoading(true);
-    e.preventDefault();
-
-    // submit free selection
-    if (selectedOption === "free") {
-      toggleSetting("isUseUserKeyEnabled", false);
-      setLoading(false);
-      showToast("Free API key selected", "bg-green");
-      onClose();
-      return;
-    }
-
-    // submit user selection
-    if (selectedOption === "user") {
-      if (inputValue === "") {
-        setLoading(false);
-        showToast("Please enter a key", "bg-red-500");
-        return;
-      }
-
-      const response = await fetch("/api/openai-test", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ key: inputValue }),
-      });
-
-      const data = await response.json();
-
-      if (data.error) {
-        setLoading(false);
-        showToast("Please enter a valid key", "bg-red-500");
-        return;
-      }
-
-      syncKey(inputValue);
-      toggleSetting("isUseUserKeyEnabled", true);
-      setLoading(false);
-      showToast("Key entered successfully", "bg-green", "text-white");
-      onClose();
-    }
-  }
-
-  const handleRemoveKey = () => {
-    syncKey("");
-    setSelectedOption("free");
-    toggleSetting("isUseUserKeyEnabled", false);
-    showToast("Key removed successfully", "bg-green", "text-white");
-  };
+  const {
+    loading,
+    inputValue,
+    handleChange,
+    handleClick,
+    handleSubmit,
+    handleRemoveKey,
+    selectedOption,
+  } = useChatSettings({ onClose });
 
   return (
     <div className="flex flex-col gap-4">
---
+++ src/hooks/useChatSettings.js
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { useToast } from "@/context/toastContext";
+import { useUserSettings } from "@/context/userSettingsContext";
+import { useSession } from "next-auth/react";
+
+export const useChatSettings = ({ onClose }) => {
+  // state
+  const { data: session } = useSession();
+  // const [loading, setLoading] = useState(false);
+  const { userSettings, toggleSetting, key, syncKey } = useUserSettings();
+  const [loading, setLoading] = useState(false);
+  const [inputValue, setInputValue] = useState(userSettings.key || "");
+  const [selectedOption, setSelectedOption] = useState(
+    !session ? "user" : userSettings.isUseUserKeyEnabled ? "user" : "free"
+  );
+
+  const showToast = useToast();
+
+  // handlers
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleClick = (option) => {
+    setSelectedOption(option);
+  };
+
+  async function handleSubmit(e) {
+    setLoading(true);
+    e.preventDefault();
+
+    // submit free selection
+    if (selectedOption === "free") {
+      toggleSetting("isUseUserKeyEnabled", false);
+      setLoading(false);
+      showToast("Free API key selected", "bg-green");
+      onClose();
+      return;
+    }
+
+    // submit user selection
+    if (selectedOption === "user") {
+      if (inputValue === "") {
+        setLoading(false);
+        showToast("Please enter a key", "bg-red-500");
+        return;
+      }
+
+      const response = await fetch("/api/openai-test", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ key: inputValue }),
+      });
+
+      const data = await response.json();
+
+      if (data.error) {
+        setLoading(false);
+        showToast("Please enter a valid key", "bg-red-500");
+        return;
+      }
+
+      syncKey(inputValue);
+      toggleSetting("isUseUserKeyEnabled", true);
+      setLoading(false);
+      showToast("Key entered successfully", "bg-green", "text-white");
+      onClose();
+    }
+  }
+
+  const handleRemoveKey = () => {
+    syncKey("");
+    setSelectedOption("free");
+    toggleSetting("isUseUserKeyEnabled", false);
+    showToast("Key removed successfully", "bg-green", "text-white");
+  };
+
+  return {
+    loading,
+    inputValue,
+    setInputValue,
+    handleChange,
+    handleClick,
+    handleSubmit,
+    handleRemoveKey,
+    selectedOption,
+  };
+};
